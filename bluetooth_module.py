5ed357e9df4c58efc4e7ed35e2b4c46c156d3e89
hansmariusoveras <47254808+hansmariusoveras@users.noreply.github.com>
2022-04-09 14:25:15 +0200

    ..

--- bluetooth_module.py
+++ bluetooth_module.py
@@ -1,9 +1,24 @@
 import pyttsx3
 import bluetooth
-nearby_devices = bluetooth.discover_devices(duration=1, lookup_names=True,
-                flush_cache=True, lookup_class=False)
-
+devices = {} 
 engine = pyttsx3.init()
-for device in nearby_devices:
-    engine.say(f"Welcome, {device[1]}")
-engine.runAndWait()
+
+voices = engine.getProperty('voices')
+for voice in voices:
+    if voice.languages[0] == u'no_NB':
+        engine.setProperty('voice', voice.id)
+        break
+
+while True:
+    
+    nearby_devices = bluetooth.discover_devices(duration=1, lookup_names=True,
+                    flush_cache=True, lookup_class=False)
+    for device in nearby_devices:
+        if not (device[0] in devices):
+            devices[device[0]] = {'name': 'Ukjent', 'deviceName': device[1]}
+            engine.say("Ny besøkende registrert. Vennligst registrer i appen.")
+    
+    for device in nearby_devices:
+        name = devices[device[0]]["name"]
+        engine.say(f"Velkommen, {name}")
+    engine.runAndWait()
