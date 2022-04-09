33180fc419d05f333425c5531813af471f37e9d6
Halvor Øverby <halvorov@stud.ntnu.no>
2022-04-09 13:42:18 +0200

    laget ui

---
+++ ui.py
@@ -0,0 +1,19 @@
+# Import and initialize the pygame library
+import pygame
+pygame.init()
+ 
+# Set up the drawing window
+screen = pygame.display.set_mode([500, 500])
+
+# Run until the user asks to quit
+running = True
+while running: 
+# Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((248, 249, 249))
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+  # Flip the display
+    pygame.display.flip()
+pygame.quit()
