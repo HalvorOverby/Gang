7beddc09baeecc85bee8af502cc3a6aa5425244d
hansmariusoveras <47254808+hansmariusoveras@users.noreply.github.com>
2022-04-09 14:46:02 +0200

    try to fix bug

--- bluetooth_module.py
+++ bluetooth_module.py
@@ -8,22 +8,24 @@ engine = pyttsx3.init()
 
 
 while True:
-    print("Current visitors:")
-    [print(dvs) for dvs in currently_visiting]
     print()
     nearby_devices = bluetooth.discover_devices(duration=1, lookup_names=True,
                     flush_cache=True, lookup_class=False)
     for device in nearby_devices:
         if not (device[0] in devices):
             devices[device[0]] = {'name': device[1], 'deviceName': device[1]}
+            print(f"Registred new device {device[1]}")
+
         if not (device[0] in currently_visiting):
             currently_visiting.add(device[0])
             name = devices[device[0]]["name"]
             engine.say(f"Welcome, {name}")
-
+    
     for device in currently_visiting.copy():
         if device not in [d[0] for d in nearby_devices]:
             currently_visiting.remove(device)
             engine.say(f"Goodbye, {name}")
     engine.runAndWait()
+    print("Current visitors:")
+    [print(dvs) for dvs in currently_visiting]
     time.sleep(5)
