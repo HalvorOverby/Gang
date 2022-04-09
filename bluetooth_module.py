3e3106cc797a3f0a4c86ef5b69dcd72c86cccb4c
hansmariusoveras <47254808+hansmariusoveras@users.noreply.github.com>
2022-04-09 14:14:04 +0200

    sound

--- bluetooth_module.py
+++ bluetooth_module.py
@@ -2,9 +2,8 @@ import pyttsx3
 import bluetooth
 nearby_devices = bluetooth.discover_devices(duration=8, lookup_names=True,
                 flush_cache=True, lookup_class=False)
-print(nearby_devices)
 
 engine = pyttsx3.init()
-name = "Halvor"
-engine.say(f"Welcome, {name}")
+for device in nearby_devices:
+    engine.say(f"Welcome, {device[1]}")
 engine.runAndWait()
