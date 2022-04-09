a9262bfca4de1e14c187f9a8fd12c8454a4ff885
Halvor Øverby <halvorov@stud.ntnu.no>
2022-04-09 14:45:52 +0200

    startet på ui

--- ui.py
+++ ui.py
@@ -1,19 +1,42 @@
-# Import and initialize the pygame library
+import datetime
 import pygame
 pygame.init()
- 
-# Set up the drawing window
-screen = pygame.display.set_mode([500, 500])
+def ui(overskrift: str, underskrift:str,startTid: datetime.datetime,vær: str):
+    white = (255, 255, 255)
+    offwhite= (237,237,236)
+    black= (0,0,0)
+    gray= (116,118,136)
+    
+    X = 960    
+    Y = 540
 
-# Run until the user asks to quit
-running = True
-while running: 
-# Did the user click the window close button?
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    screen.fill((248, 249, 249))
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-  # Flip the display
-    pygame.display.flip()
-pygame.quit()
+    marg=10
+    largeFontSize=100
+    smallFontSize=50
+    
+    display_surface = pygame.display.set_mode((X, Y))
+    tittelfont = pygame.font.Font("freesansbold.ttf", largeFontSize)
+    underoverskriftfont= pygame.font.Font("freesansbold.ttf",smallFontSize)
+    
+    tittel = tittelfont.render(overskrift, True, black, offwhite)
+    underoverskrift= underoverskriftfont.render(underskrift,True,gray,offwhite)
+    
+    Recttittel=tittel.get_rect()
+    Rectunderoverskrift=underoverskrift.get_rect()
+
+    Recttittel.topleft = (marg, marg)
+    Rectunderoverskrift.topleft = (marg,2*marg+largeFontSize)
+    
+
+    while True:
+        display_surface.fill(offwhite)
+        display_surface.blit(tittel, Recttittel)
+        display_surface.blit(underoverskrift,Rectunderoverskrift)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        pygame.display.update()
+
+ui("God morgen :D","Memento mori",datetime.datetime.now(),"sol")
